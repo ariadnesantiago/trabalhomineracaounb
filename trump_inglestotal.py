7c2deb75dbb36a6e13351df229316e024f018391
ariadnesantiago <ariadne.osantiago@gmail.com>
2016-11-20 01:46:31 -0200

    Código para base de dados completa processada
    Código para processar tweets de #trumpwins:
    - Retirada de links e emoticons
    - Apenas textos em inglês

---
+++ trump_inglestotal.py
@@ -0,0 +1,37 @@
+#### CODIGO PARA CRIAR BASE DE DADOS SELECIONADA POR DATA E COM INFORMACOES QUE PODEM SER UTEIS
+
+import json
+import re
+import langid
+
+f = open('stream_trumpwins.txt')
+
+r = "^\{\"created_at\":\"Wed Nov 09"
+
+print('texto')
+
+for l in f:
+
+	match = re.search(r,l)
+	
+	if match:
+
+ 		obj = json.loads(l)
+ 			
+ 		if "created_at" in obj:
+ 			
+ 			id_original = obj['id']
+ 			data = obj['created_at']
+ 			usuario = obj['user']['screen_name']
+ 			localizacao = obj['user']['location']
+ 			texto = obj['text'].replace('"',"'").replace("\n"," - ").lower()
+ 			texto = re.sub(":(\w)", "\:\\1",texto) # para tirar emoticon
+ 			texto = re.sub("http.*?( |$)","",texto) # para tirar links
+ 			texto = re.sub("^rt @.*?: ","",texto) # pra tirar inicio do tweet que mostra ser retweet
+ 			
+ 			#selecionar lingua inglesa
+ 			if langid.classify(texto)[0] == 'en':
+ 			
+ 				print(('%s,%s,%s,%s,%s') % (id_original, data, usuario, localizacao, texto.encode('utf-8').decode('utf-8').encode('cp1252','ignore').decode('cp1252')))
+ 
+# codigo para amostra aleatoria bash : -n 40000 trumpwins.csv > amostratrumpwins40000.csv
